ec155751d1bb894429f921122459dbf9a2ad15fe
haydenmosesth-collab <haydenmosesth@gmail.com>
2026-09-01 20:10:04 +0530

    Add files via upload

---
+++ src/combinestring/CombineString.java
@@ -0,0 +1,15 @@
+package combinestring;
+import java.util.Scanner;
+
+public class CombineString {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		String s1 = sc.next();
+		String s2 = sc.next();
+		String result = s1 + s2;
+		System.out.println(result);
+	}
+	
+
+}
