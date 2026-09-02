dd833fefbb8fa12e9eb782ceecfe212d47fcced8
haydenmosesth-collab <haydenmosesth@gmail.com>
2026-09-02 20:37:57 +0530

    Add files via upload

---
+++ FibonacciValue.java
@@ -0,0 +1,24 @@
+package fibonaccivalue;
+
+import java.util.Scanner;
+
+public class FibonacciValue {
+	static int fib(int n) {
+		if(n == 0) {
+			return 0;
+		}
+		if(n == 1) {
+			return 1;
+		}
+		return fib(n-1) + fib(n-2);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Fibonacci Value = " +fib(n));
+		sc.close();
+
+	}
+
+}
